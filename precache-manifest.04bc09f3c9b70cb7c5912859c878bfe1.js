c7dfd412213e6482c5d09ec776cda212af50a0fd
shotteammate <shotteammate@gmail.com>
2019-11-04 17:40:15 +0800

    Updates

---
+++ precache-manifest.04bc09f3c9b70cb7c5912859c878bfe1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f8d5e4815e800b4f6ab68b36d43d441",
+    "url": "/appstoredemo/index.html"
+  },
+  {
+    "revision": "b3368fb875fc169f5947",
+    "url": "/appstoredemo/static/css/main.18e89592.chunk.css"
+  },
+  {
+    "revision": "95707cedeff069b47dd2",
+    "url": "/appstoredemo/static/js/2.7af6d206.chunk.js"
+  },
+  {
+    "revision": "b3368fb875fc169f5947",
+    "url": "/appstoredemo/static/js/main.d84ff32e.chunk.js"
+  },
+  {
+    "revision": "498c2363bf70090a2d6f",
+    "url": "/appstoredemo/static/js/runtime-main.12aba0e8.js"
+  }
+]);
